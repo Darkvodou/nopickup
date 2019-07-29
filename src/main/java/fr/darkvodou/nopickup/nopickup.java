c49b98f5f38157d4ee5c65e65de3a1b77fa0fa4a
Bernert <facebookger@live.fr>
2019-07-29 20:12:10 +0200

    First upload

---
+++ src/main/java/fr/darkvodou/nopickup/nopickup.java
@@ -0,0 +1,22 @@
+package fr.darkvodou.nopickup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class nopickup extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Heeeyyyyyy je suis allumé");
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
+
+
+
+
